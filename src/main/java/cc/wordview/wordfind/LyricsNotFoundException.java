c37deeae8a6c01e4d4e4bcd276fbe363aa7a11c2
Arthur Araujo <arthur.araujo@tutanota.com>
2024-10-31 12:01:55 -0300

    Delete old java directories

--- src/main/java/cc/wordview/wordfind/LyricsNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package cc.wordview.wordfind;
-
-public class LyricsNotFoundException extends Exception {
-        public LyricsNotFoundException(String message) {
-                super(message);
-        }
-}
-
